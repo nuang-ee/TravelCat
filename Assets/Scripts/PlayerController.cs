e45b4f056fd0fc98095a6bb3163a155d34382ef1
Nuang_ee <random0411@kaist.ac.kr>
2020-01-17 01:18:15 +0900

    Upload Cute Cat

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Character Attributes:")]
+    public float MOVEMENT_BASE_SPEED = 1.0f;
+
+    [Space]
+    [Header("Character Statistics:")]
+    public Vector2 movementDirection;
+    public float movementSpeed;
+
+    [Space]
+    [Header("References:")]
+    public Rigidbody2D rigidbody;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        Move();
+        Animate();
+    }
+
+    void ProcessInputs() {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+    }
+
+    void Move() {
+        rigidbody.velocity = movementDirection * movementSpeed * MOVEMENT_BASE_SPEED;
+    }
+
+    void Animate() {
+        animator.SetFloat("Horizontal", movementDirection.x);
+        animator.SetFloat("Vertical", movementDirection.y);
+        animator.SetFloat("Magnitude", movementSpeed);
+    }
+}
